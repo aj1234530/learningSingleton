15bb5f36d52987b3134e2ef4b01acca5521a7c6d
mainabhihun <youmailmejust123@gmail.com>
2024-12-04 21:45:38 +0530

    added types to games(in memory-var) via interface

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,9 @@
 //thing to learn is store
 //how to introduce classes and then how to intruduce singleton on top of that
-
-export const games: object[] = []
+interface Game{
+    id: string,
+    player1: string,
+    player2: string,
+    moves: string []
+}
+export const games: Game[] = []
